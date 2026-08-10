aa00f138090fde3e9364fbed0a481386cfc68cac
Pedro Henrique <phpm13@gmail.com>
2026-08-10 18:42:38 -0300

    Apply Casey Bodley's suggestions

--- src/rgw/driver/rados/rgw_bucket.cc
+++ src/rgw/driver/rados/rgw_bucket.cc
@@ -302,7 +302,7 @@ static void dump_bucket_index(const vector<rgw_bucket_dir_entry>& objs,  Formatt
   }
 }
 
-static void dump_bucket_usage(map<RGWObjCategory, RGWStorageStats>& stats, std::optional<std::map<std::string, RGWStorageStats>> sc_stats, Formatter *formatter)
+static void dump_bucket_usage(map<RGWObjCategory, RGWStorageStats>& stats, std::optional<std::map<std::string, RGWStorageClassStats>> sc_stats, Formatter *formatter)
 {
   map<RGWObjCategory, RGWStorageStats>::iterator iter;
 
@@ -316,7 +316,7 @@ static void dump_bucket_usage(map<RGWObjCategory, RGWStorageStats>& stats, std::
   if (sc_stats.has_value()) {
     formatter->open_array_section("rgw.storage-classes");
     for (auto it = sc_stats.value().begin(); it != sc_stats.value().end(); ++it){
-      RGWStorageStats& s = it->second;
+      RGWStorageClassStats& s = it->second;
       std::string storage_class = it->first;
       formatter->open_object_section(storage_class);
       formatter->dump_string("name", storage_class);
@@ -333,7 +333,7 @@ static void dump_index_check(map<RGWObjCategory, RGWStorageStats> existing_stats
         map<RGWObjCategory, RGWStorageStats> calculated_stats,
         Formatter *formatter)
 {
-  std::optional<std::map<std::string, RGWStorageStats>> sc_stats;
+  std::optional<std::map<std::string, RGWStorageClassStats>> sc_stats;
   formatter->open_object_section("check_result");
   formatter->open_object_section("existing_header");
   dump_bucket_usage(existing_stats, sc_stats, formatter);
@@ -1611,8 +1611,8 @@ static int bucket_stats(rgw::sal::Driver* driver, const rgw::SiteConfig& site,
                         const DoutPrefixProvider* dpp, optional_yield y) {
   std::unique_ptr<rgw::sal::Bucket> bucket;
   map<RGWObjCategory, RGWStorageStats> stats;
-  std::optional<std::map<std::string, RGWStorageStats>> sc_stats{
-    std::map<std::string, RGWStorageStats>{}
+  std::optional<std::map<std::string, RGWStorageClassStats>> sc_stats{
+    std::map<std::string, RGWStorageClassStats>{}
   };
 
   int ret = driver->load_bucket(dpp, rgw_bucket(tenant_name, bucket_name),
@@ -1640,12 +1640,6 @@ static int bucket_stats(rgw::sal::Driver* driver, const rgw::SiteConfig& site,
       cerr << "error getting bucket stats bucket=" << bucket->get_name() << " ret=" << ret << std::endl;
       return ret;
     }
-  }
-
-  auto radosdriver = static_cast<rgw::sal::RadosStore*>(driver);
-  if (!radosdriver) {
-    cerr << "rados store only" << std::endl;
-    return -ENOTSUP;
   }
 
   utime_t ut(bucket->get_modification_time());
@@ -1764,8 +1758,8 @@ int RGWBucketAdminOp::limit_check(rgw::sal::Driver* driver,
 	/* need stats for num_entries */
 	string bucket_ver, master_ver;
 	std::map<RGWObjCategory, RGWStorageStats> stats;
-  std::optional<std::map<std::string, RGWStorageStats>> sc_stats{
-    std::map<std::string, RGWStorageStats>{}
+  std::optional<std::map<std::string, RGWStorageClassStats>> sc_stats{
+    std::map<std::string, RGWStorageClassStats>{}
   };
 	ret = bucket->read_stats(dpp, y, index, RGW_NO_SHARD, &bucket_ver, &master_ver, stats, sc_stats, nullptr);
 
@@ -3684,7 +3678,6 @@ int RGWBucketCtl::sync_owner_stats(const DoutPrefixProvider *dpp,
                                    const rgw_owner& owner,
                                    const RGWBucketInfo& bucket_info,
                                    optional_yield y,
-                                   bool reset,
                                    RGWBucketEnt* pent)
 {
   RGWBucketEnt ent;
@@ -3706,7 +3699,7 @@ int RGWBucketCtl::sync_owner_stats(const DoutPrefixProvider *dpp,
         const RGWZoneParams& zone = svc.zone->get_zone_params();
         return rgwrados::account::get_buckets_obj(zone, id);
       }), owner);
-  return rgwrados::buckets::write_stats(dpp, y, rados, obj, *pent, reset);
+  return rgwrados::buckets::write_stats(dpp, y, rados, obj, *pent);
 }
 #endif
 
